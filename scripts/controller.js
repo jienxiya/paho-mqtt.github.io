fa55aad2280c75a7b657e1ea599bfcd2aaf7a26d
jienxiya <jeinxiya@gmail.com>
2019-09-03 16:58:28 +0800

    Set brokerto wss://test.mosquito.org:8081/mqtt

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,5 +1,5 @@
 $('#btn-connect').click(function(){
-  client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt");
+  client = mqtt.connect("wss://test.mosquito.org:8081/mqtt");
   client.subscribe($("#topic").val());
   console.log('connect button when clicked');
    $("#status").text("Connecting...");
