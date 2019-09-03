32b0123be35d41ec5d1928f3750e2e47c22f7de4
jienxiya <jeinxiya@gmail.com>
2019-09-03 17:29:06 +0800

    Set local location to online

--- scripts/controller.js
+++ scripts/controller.js
@@ -5,95 +5,102 @@ $('#btn-connect').click(function(){
    $("#status").text("Connecting...");
   
   client.on("connect", function(){
-     $("#status").text("Successfully connected!!!!");
+    //  $("#status").text("Successfully connected!!!!");
    
-    Swal.fire({
-      position: 'center',
-      type: 'success!!',
-      title: 'your sucessfully connected to the broker!',
-      showConfirmButton: false,
-      timer: 2000
-      })
+    // Swal.fire({
+    //   position: 'center',
+    //   type: 'success!!',
+    //   title: 'your sucessfully connected to the broker!',
+    //   showConfirmButton: false,
+    //   timer: 2000
+    //   })
     console.log("success");
   });
 
   $("#btn-disconnect").click(function() {
-    Swal.fire({
-      title: 'Are you sure?',
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes!'
-      }).then((result) => {
-      if (result.value) {
-        client.end();
-        Swal.fire(
-        'Disconnected!',
-        'Your are disconnected to the broker.',
-        'success'
-        );
-        $("#status").text("Disconnected");
-        // $("#status").removeClass("alert-warning");
-        // $("#status").addClass("alert-secondary");
-      }
-      })
+    // Swal.fire({
+    //   title: 'Are you sure?',
+    //   type: 'warning',
+    //   showCancelButton: true,
+    //   confirmButtonColor: '#3085d6',
+    //   cancelButtonColor: '#d33',
+    //   confirmButtonText: 'Yes!'
+    //   }).then((result) => {
+    //   if (result.value) {
+    //     client.end();
+    //     Swal.fire(
+    //     'Disconnected!',
+    //     'Your are disconnected to the broker.',
+    //     'success'
+    //     );
+    //     $("#status").text("Disconnected");
+    //     // $("#status").removeClass("alert-warning");
+    //     // $("#status").addClass("alert-secondary");
+    //   }
+    //   })
+    client.end();
+    console.log("Disconnected")
     
   });
 
   $("#btn-publish").click(function() {
     var topic = $("#topic").val();
     var payload = $("#message").val();
-    if (topic == "" && payload == "") {
-      Swal.fire({
-        type: 'error',
-        title: 'Oops...',
-        text: 'Please provide inputs!',
-      });
-    }
-    else { 
-      client.publish(topic,payload, function(err) {
-          if (err){
-          Swal.fire({
-            type: 'error',
-            title: 'Oops...',
-            text: 'There is an error!',
-            });
-        } else {
-          console.log("published")
-          Swal.fire(' topic has been published successfully!')
-        }
-      });
+  //   if (topic == "" && payload == "") {
+  //     Swal.fire({
+  //       type: 'error',
+  //       title: 'Oops...',
+  //       text: 'Please provide inputs!',
+  //     });
+  //   }
+  //   else { 
+  //     client.publish(topic,payload, function(err) {
+  //         if (err){
+  //         Swal.fire({
+  //           type: 'error',
+  //           title: 'Oops...',
+  //           text: 'There is an error!',
+  //           });
+  //       } else {
+  //         console.log("published")
+  //         Swal.fire(' topic has been published successfully!')
+  //       }
+  //     });
       
       
-    }
+  //   }
 
-  });
+  console.log("Successfully Published")
+  console.log("Published { topic: " + topic
+  + "; payload: " + payload + " }");
+
+});
+
   $("#btn-subscribe").click(function() {
     var subscribe = $("#topic-sub").val();
     var topic = $("#topic").val();
-    if (subscribe != topic) {
-      Swal.fire({
-        type: 'error',
-        title: 'Oops...',
-        text: 'Topic is not available!',
-      });
-    }
-    else if (subscribe == topic && topic !== "") {
-      client.subscribe(topic, function(err) {
-        if(err) {
-          Swal.fire({
-            type: 'error',
-            title: 'Oops...',
-            text: 'An error occurs!',
-            });
-        } else {
-          Swal.fire('Subscribed successfully!');
-        }
-      });
+    // if (subscribe != topic) {
+    //   Swal.fire({
+    //     type: 'error',
+    //     title: 'Oops...',
+    //     text: 'Topic is not available!',
+    //   });
+    // }
+    // else if (subscribe == topic && topic !== "") {
+    //   client.subscribe(topic, function(err) {
+    //     if(err) {
+    //       Swal.fire({
+    //         type: 'error',
+    //         title: 'Oops...',
+    //         text: 'An error occurs!',
+    //         });
+    //     } else {
+    //       Swal.fire('Subscribed successfully!');
+    //     }
+    //   });
       
-    }
-      
+    // }
+      console.log("Subscribe { topic: " + subscribe + " }");
   })
   client.on("message", function (topic, payload) {
     // console.log([topic, payload].join(": "));
