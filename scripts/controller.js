e4524cfb2fa0c9a327ebe20ace0ba68f5f04c016
jienxiya <jeinxiya@gmail.com>
2019-09-09 22:05:27 +0800

    Basic Function

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,38 +1,30 @@
+
 $('#btn-connect').click(function(){
   client = mqtt.connect("wss://test.mosquitto.org:8081/mqtt");
-  client.subscribe($("#topic").val());
-  console.log('connect button when clicked');
+  // client.subscribe($("#topic").val());
    $("#status").text("Connecting...");
   
   client.on("connect", function(){
      $("#status").text("Successfully connected!");
-
+     console.log("success");
   });
-  console.log("success");
 
-  $("#btn-disconnect").click(function() {
-    Swal.fire({
-      title: 'Are you sure?',
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes!'
-      }).then((result) => {
-      if (result.value) {
-        client.end();
-        Swal.fire(
-        'Disconnected!',
-        'Your are disconnected to the broker.',
-        'success'
-        );
-        $("#status").text("Disconnected");
-        client.end();
-        }
-      })
-    client.end();
-    console.log("Disconnected")   
+    client.on("message", function (topic, payload) {
+    console.log("Received { topic: " + topic + "; payload: " + payload + " }");
+    // var row = $("<tr>");
+    // $("<td>").text(topic).appendTo($(row));
+    // $("<td>").text(payload).appendTo($(row));
+    // $("<td>").text(moment().format('MMMM Do YYYY, h:mm:ss a')).appendTo($(row));
+    // $("#tbl-body").append($(row));
   });
+});
+ 
+$("#btn-disconnect").click(function() {  
+  client.end();   
+  $("#status").text("Disconnected");
+  console.log("Disconnected")
+});
+
 
   $("#btn-publish").click(function() {
     var topic = $("#topic").val();
@@ -44,49 +36,59 @@ $('#btn-connect').click(function(){
           if (err){
           console.log("Error")
         } else {
-          console.log("published") 
+          console.log("Successfully Published")
+          console.log("Published { topic: " + topic + "; payload: " + payload + " }");
         }
       });
     }
-  console.log("Successfully Published")
-  console.log("Published { topic: " + topic
-  + "; payload: " + payload + " }");
-})
+});
 
   $("#btn-subscribe").click(function() {
     var subscribe = $("#topic-sub").val();
     var topic = $("#topic").val();
-    // if (subscribe != topic) {
-    //   Swal.fire({
-    //     type: 'error',
-    //     title: 'Oops...',
-    //     text: 'Topic is not available!',
-    //   });
-    // }
-    // else if (subscribe == topic && topic !== "") {
-    //   client.subscribe(topic, function(err) {
-    //     if(err) {
-    //       Swal.fire({
-    //         type: 'error',
-    //         title: 'Oops...',
-    //         text: 'An error occurs!',
-    //         });
-    //     } else {
-    //       Swal.fire('Subscribed successfully!');
-    //     }
-    //   });
-      
-    // }
-      console.log("Subscribe { topic: " + subscribe + " }");
-  })
+    if (subscribe != topic) {
+      console.log("Topic is not available")
+    }
+    else if (subscribe == topic && topic !== "") {
+      client.subscribe(topic, function(err) {
+        if(err) {
+          console.log("Error")
+        } else {
+          console.log("Subscribed successfully!")
+          console.log("Subscribe { topic: " + subscribe + " }");
+        }
+      });     
+    }
+  });
 
-  client.on("message", function (topic, payload) {
-    // console.log([topic, payload].join(": "));
-    var row = $("<tr>");
-    $("<td>").text(topic).appendTo($(row));
-    $("<td>").text(payload).appendTo($(row));
-    $("<td>").text(moment().format('MMMM Do YYYY, h:mm:ss a')).appendTo($(row));
-    $("#tbl-body").append($(row));
+  $("#btn-unsubsribe").click(function() {
+            var topic = $("#topic").val();
+            client.unsubscribe(topic, function(err) {
+                if(err) {
+                    Swal.fire({
+                        type: 'error',
+                        title: 'Oops...',
+                        text: 'An error occurs!',
+                        });
+                } else {
+                    swal.fire({
+                        title: 'Confirm',
+                        text: 'Are you sure you want to unsubscribe?',
+                        type: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: 'green'
+                 });
+                }
+            });
+    })
 
-  })
-});
+  // client.on("message", function (topic, payload) {
+  //   // console.log([topic, payload].join(": "));
+  //   console.log("Received { topic: " + topic + "; payload: " + payload + " }");
+  //   var row = $("<tr>");
+  //   $("<td>").text(topic).appendTo($(row));
+  //   $("<td>").text(payload).appendTo($(row));
+  //   $("<td>").text(moment().format('MMMM Do YYYY, h:mm:ss a')).appendTo($(row));
+  //   $("#tbl-body").append($(row));
+  // })
+// });
